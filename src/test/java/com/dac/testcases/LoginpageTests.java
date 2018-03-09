61c9d6704903a6295ea5c55ca82245fc1dac71ae
rahuln <rahuln@LapWinX-DAC8.arackal.co.in>
2018-03-09 16:34:08 +0530

    changed extend report theme,created two test files

--- src/test/java/com/dac/testcases/LoginpageTests.java
+++ src/test/java/com/dac/testcases/LoginpageTests.java
@@ -12,61 +12,42 @@ import com.relevantcodes.extentreports.LogStatus;
 
 import resources.base;
 
-
-
-
 public class LoginpageTests extends base {
 
-	
 	LoginPage lp;
-	
+
 	@BeforeTest
 	public void setup() throws IOException {
 		driver = initializeDriver();
-	    lp= new LoginPage(driver);
+		lp = new LoginPage(driver);
 	}
-	
-	@Test(priority=1)
+
+	@Test
 	public void invalid_login() {
 		try {
-		driver.get(prop.getProperty("url"));
-		lp.login().clear();
-		lp.login().sendKeys("dac_ra_beta1@dacgroup.com");
-		lp.password().clear();
-		lp.password().sendKeys("1111");
-		lp.submit().click();
-		Thread.sleep(6000);
-		logger.log(LogStatus.INFO, "asserting validation message");
-		Assert.assertEquals("Invalid Password2", lp.warning().getAttribute("innerText"));
-		
+			driver.get(prop.getProperty("url"));
+			lp.login().clear();
+			lp.login().sendKeys("dac_ra_beta1@dacgroup.com");
+			lp.password().clear();
+			lp.password().sendKeys("1111");
+			lp.submit().click();
+			Thread.sleep(6000);
+			logger.log(LogStatus.INFO, "asserting validation message");
+			Assert.assertEquals("Invalid Password2", lp.warning().getAttribute("innerText"));
+
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
-	@Test(priority=2)
-	public void valid_login() throws InterruptedException {
-		
-		
-		lp.login().clear();
-		lp.login().sendKeys("dac_ra_beta1@dacgroup.com");
-		lp.password().clear();
-		lp.password().sendKeys("111111");
-		lp.submit().click();
-		Thread.sleep(6000);
-	    boolean test= lp.logo().isDisplayed();
-	    logger.log(LogStatus.INFO, "asserting logo displayed");
-		Assert.assertFalse(test);
-		
-		}
-	
-		@AfterTest
-		public void tearDownMethod() throws Exception {
-			driver.close();
-			driver=null;
-			
-		}
+
+
+	@AfterTest
+	public void tearDownMethod() throws Exception {
+		driver.close();
+		driver = null;
+
+	}
 }
